781c04d2d0a8e610a270a9dea9825e9805155e43
shinyay <shinya.com@gmail.com>
2021-11-08 22:33:34 +0900

    Add update employee

--- src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
@@ -3,15 +3,20 @@ package io.spring.shinyay.repository
 import io.spring.shinyay.entity.Employee
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 
 @Repository
 class EmployeeRepository {
 
-    var employeeData: Map<String, Employee>? = null
-    var employeeAccessData: Map<String, String>? = null
+    var employeeData: Map<Long, Employee>? = null
+    var employeeAccessData: Map<Long, String>? = null
 
     fun findAllEmployees(): Flux<Employee> {
         return Flux.fromIterable(employeeData!!.values)
+    }
+
+    fun updateEmployee(employee: Employee): Mono<Employee> {
+        return Mono.just(employee)
     }
 }
