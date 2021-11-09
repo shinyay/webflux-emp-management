67f013f56c2562fbe90ad1340560e31beb3b967a
shinyay <shinya.com@gmail.com>
2021-11-09 16:06:29 +0900

    Add Router function model with Flux endpoint

--- src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
@@ -2,9 +2,13 @@ package io.spring.shinyay.controller
 
 import io.spring.shinyay.entity.Employee
 import io.spring.shinyay.repository.EmployeeRepository
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RouterFunctions.route
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.ZoneId
@@ -18,13 +22,26 @@ class EmployeeController(val repository: EmployeeRepository) {
     @GetMapping("/hello-mono")
     fun helloMono() = Mono.just("Hello Mono")
 
-    @GetMapping("/hello-flux")
-    fun helloFlux() = Flux.just("Hello","Flux","at",
-        ZonedDateTime.now(
-            ZoneId.of("Japan")
-        ).format(
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-        ))
+    @Bean
+    fun routeHelloFlux(): RouterFunction<ServerResponse?>? {
+        return route(GET("/hello-flux")) { this.helloFlux() }
+    }
+
+    fun helloFlux(): Mono<ServerResponse?> {
+        return ServerResponse.ok().body(
+            Flux.just(
+                "Hello",
+                "Flux",
+                "at",
+                ZonedDateTime
+                    .now(
+                        ZoneId.of("Japan")
+                    ).format(
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+                    )
+            ), String::class.java
+        )
+    }
 
     @GetMapping("/employees")
     fun getAllEmployees(): Flux<Employee> {
