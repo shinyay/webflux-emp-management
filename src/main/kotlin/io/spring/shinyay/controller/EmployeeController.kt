975f3d31de2b285888b66e8ac8270e452857d007
shinyay <shinya.com@gmail.com>
2021-11-09 14:59:31 +0900

    Add Get endpoint to find all employees

--- src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
@@ -1,9 +1,18 @@
 package io.spring.shinyay.controller
 
+import io.spring.shinyay.entity.Employee
+import io.spring.shinyay.repository.EmployeeRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @RestController
 @RequestMapping("/api/v1")
-class EmployeeController {
+class EmployeeController(val repository: EmployeeRepository) {
+
+    @GetMapping("/employees")
+    fun getAllEmployees(): Flux<Employee> {
+        return repository.findAllEmployees()
+    }
 }
