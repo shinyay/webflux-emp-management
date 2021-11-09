9d6a6ef20bbfcf5e7396e4f7c23e83b66eb226ec
shinyay <shinya.com@gmail.com>
2021-11-09 15:29:28 +0900

    Add Mono sample

--- src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/EmployeeController.kt
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api/v1")
 class EmployeeController(val repository: EmployeeRepository) {
+
+    @GetMapping("/hello")
+    fun helloMono () = Mono.just("Hello Mono")
 
     @GetMapping("/employees")
     fun getAllEmployees(): Flux<Employee> {
