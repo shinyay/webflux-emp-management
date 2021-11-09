4a833ceecff9919c2550d4960c0acad7f9abe41e
shinyay <shinya.com@gmail.com>
2021-11-09 15:06:05 +0900

    Add WebClient for Employee Flux endpoint

---
+++ src/main/kotlin/io/spring/shinyay/client/EmployeeWebClient.kt
@@ -0,0 +1,19 @@
+package io.spring.shinyay.client
+
+import io.spring.shinyay.entity.Employee
+import org.springframework.web.reactive.function.client.WebClient
+
+class EmployeeWebClient {
+
+    private val client = WebClient.create("http://localhost:8080")
+
+    fun consumeEmployee() {
+        val employeeFlux = client.get()
+            .uri("/api/v1/employees")
+            .retrieve()
+            .bodyToFlux(Employee::class.java)
+        employeeFlux.subscribe { employee -> println(employee) }
+        }
+    }
+
+}
