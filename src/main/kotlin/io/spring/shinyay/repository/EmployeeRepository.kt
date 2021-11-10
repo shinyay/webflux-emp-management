6bc2a5075db7bfa167d839bf329dca647cc338c0
shinyay <shinya.com@gmail.com>
2021-11-10 15:38:13 +0900

    Modify it as interface from clas

--- src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
@@ -1,22 +1,11 @@
 package io.spring.shinyay.repository
 
 import io.spring.shinyay.entity.Employee
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 
 @Repository
-class EmployeeRepository {
-
-    var employeeData: Map<Long, Employee>? = null
-    var employeeAccessData: Map<Long, String>? = null
-
-    fun findAllEmployees(): Flux<Employee> {
-        return Flux.fromIterable(employeeData!!.values)
-    }
-
-    fun updateEmployee(employee: Employee): Mono<Employee> {
-        return Mono.just(employee)
-    }
-}
+interface EmployeeRepository : ReactiveCrudRepository<Employee, Long>
