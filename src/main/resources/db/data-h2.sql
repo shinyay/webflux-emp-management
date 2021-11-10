1ce5c417c8e677fb5643db257a161153783dabbd
shinyay <shinya.com@gmail.com>
2021-11-10 20:02:20 +0900

    Add test data

--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -4,4 +4,7 @@ INSERT INTO department VALUES (30, 'SALES');
 INSERT INTO department VALUES (40, 'OPERATIONS');
 INSERT INTO department VALUES (100, 'ENGINEERING');
 
-INSERT INTO employee VALUES (1, 100, 'Alice', 'tester');
+INSERT INTO employee VALUES (1, 10, 'John', 'Dev');
+INSERT INTO employee VALUES (2, 20, 'Mary', 'QA');
+INSERT INTO employee VALUES (3, 30, 'Peter', 'Tester');
+INSERT INTO employee VALUES (4, 100, 'Mike', 'Designer');
