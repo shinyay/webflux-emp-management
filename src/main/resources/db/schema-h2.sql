36acf1595fad9ecc1d6b87dae2791b7b9f917efa
shinyay <shinya.com@gmail.com>
2021-11-10 18:25:55 +0900

    Add schema file to create database

---
+++ src/main/resources/db/schema-h2.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+                            department_id decimal(4,0) NOT NULL AUTO_INCREMENT,
+                            department_name varchar(14) DEFAULT NULL,
+                            PRIMARY KEY (department_id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+                          employee_id decimal(4,0) NOT NULL AUTO_INCREMENT,
+                          department_id decimal(4,0),
+                          employee_name varchar(64) NOT NULL,
+                          role varchar(32) DEFAULT NULL,
+                          PRIMARY KEY (employee_id)
+);
+
+ALTER TABLE employee ADD FOREIGN KEY (department_id) REFERENCES department (department_id);
