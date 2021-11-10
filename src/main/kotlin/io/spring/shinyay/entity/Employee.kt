3655b62360118ef7af3bb6e690302290320434b6
shinyay <shinya.com@gmail.com>
2021-11-10 18:24:46 +0900

    Add propeties for Employee

--- src/main/kotlin/io/spring/shinyay/entity/Employee.kt
+++ src/main/kotlin/io/spring/shinyay/entity/Employee.kt
@@ -1,9 +1,13 @@
 package io.spring.shinyay.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
+@Table("employee")
 data class Employee(
     @Id
-    var id: Long,
-    var name: String
+    val employee_id: Long,
+    val department_id: Long,
+    val name: String,
+    val role: String
 )
