2d93af6a91e8b69a09f2bc13ce0535e0ad8f62ef
shinyay <shinya.com@gmail.com>
2021-11-10 18:25:11 +0900

    Modify import to optimize

--- src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
@@ -3,9 +3,6 @@ package io.spring.shinyay.repository
 import io.spring.shinyay.entity.Employee
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-
 
 @Repository
 interface EmployeeRepository : ReactiveCrudRepository<Employee, Long>
