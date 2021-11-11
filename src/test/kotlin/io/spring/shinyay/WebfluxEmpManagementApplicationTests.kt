8256fa907399478592062a20157b4abd52fdb718
shinyay <shinya.com@gmail.com>
2021-11-11 13:41:04 +0900

    Add testing for findall element

--- src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
+++ src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
@@ -1,11 +1,13 @@
 package io.spring.shinyay
 
+import io.spring.shinyay.entity.Employee
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.test.StepVerifier
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebfluxEmpManagementApplicationTests(@LocalServerPort val port: Int) {
@@ -13,7 +15,23 @@ class WebfluxEmpManagementApplicationTests(@LocalServerPort val port: Int) {
 	val client: WebTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:$port").build()
 
 	@Test
-	fun addEmployee() {
-        client.get().uri("/api/v1/employees").exchange().expectStatus().isOk
+	fun Given_LocalH2_When_AccessFluxAPI_Then_Return_OK() {
+        client.get().uri("/api/v1/employees")
+			.exchange()
+			.expectStatus()
+			.isOk
+	}
+
+	@Test
+	fun Given_LocalH2_When_FindAll_Then_Return_Element() {
+		val employee = client.get().uri("/api/v1/employees")
+			.exchange()
+			.expectStatus()
+			.isOk
+			.returnResult(Employee::class.java)
+			.responseBody
+		StepVerifier.create(employee)
+            .expectNextCount(8)
+            .verifyComplete()
 	}
 }
