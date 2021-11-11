3abc231c799b219e23ed3c71d32cc0c60f81de50
shinyay <shinya.com@gmail.com>
2021-11-11 20:58:39 +0900

    Add custome query function with query annotation

--- src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/io/spring/shinyay/repository/EmployeeRepository.kt
@@ -1,8 +1,14 @@
 package io.spring.shinyay.repository
 
 import io.spring.shinyay.entity.Employee
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface EmployeeRepository : ReactiveCrudRepository<Employee, Long>
+interface EmployeeRepository : ReactiveCrudRepository<Employee, Long> {
+
+    @Query("select e from Employee e where e.department_id = :departmentId")
+    fun findByDepartmentId(departmentId: Long): Flux<Employee>
+}
