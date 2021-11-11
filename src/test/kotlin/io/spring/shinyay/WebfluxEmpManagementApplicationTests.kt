7bf4419e8142a28576989179718377e30a25ad46
shinyay <shinya.com@gmail.com>
2021-11-11 14:03:00 +0900

    Add find employee by id test

--- src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
+++ src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.test.StepVerifier
 
@@ -34,4 +35,19 @@ class WebfluxEmpManagementApplicationTests(@LocalServerPort val port: Int) {
             .expectNextCount(8)
             .verifyComplete()
 	}
+
+	@Test
+	fun Given_LocalH2_When_FindById_Then_Return_Element() {
+        val employee = client.get().uri("/api/v1/employees/1")
+            .exchange()
+            .expectStatus()
+            .isOk
+            .returnResult(Employee::class.java)
+            .responseBody
+        StepVerifier.create(employee)
+            .expectNextCount(1)
+            .verifyComplete()
+    }
+
+
 }
