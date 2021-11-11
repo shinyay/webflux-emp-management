6249a5f7ce881260110cb3f6718af69194df300d
shinyay <shinya.com@gmail.com>
2021-11-11 13:24:46 +0900

    Add test class

--- src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
+++ src/test/kotlin/io/spring/shinyay/WebfluxEmpManagementApplicationTests.kt
@@ -1,13 +1,19 @@
 package io.spring.shinyay
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class WebfluxEmpManagementApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebfluxEmpManagementApplicationTests(@LocalServerPort val port: Int) {
+
+	val client: WebTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:$port").build()
 
 	@Test
-	fun contextLoads() {
+	fun addEmployee() {
+        client.get().uri("/api/v1/employees").exchange().expectStatus().isOk
 	}
-
 }
