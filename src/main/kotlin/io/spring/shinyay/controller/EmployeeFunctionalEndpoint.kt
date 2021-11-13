53a2ea5a8bb5e1fce641eb083dcddd9f4c3b011a
shinyay <shinya.com@gmail.com>
2021-11-13 10:53:14 +0900

    Add handler for findAll

--- src/main/kotlin/io/spring/shinyay/controller/EmployeeFunctionalEndpoint.kt
+++ src/main/kotlin/io/spring/shinyay/controller/EmployeeFunctionalEndpoint.kt
@@ -1,4 +1,22 @@
 package io.spring.shinyay.controller
 
-class EmployeeFunctionalEndpoint {
+import io.spring.shinyay.entity.Employee
+import io.spring.shinyay.repository.EmployeeRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+class EmployeeFunctionalEndpoint(val repository: EmployeeRepository) {
+
+
+
+    fun findAllEmployees(request: ServerRequest): Mono<ServerResponse> {
+        val result: Flux<Employee> = repository.findAll()
+        return ServerResponse.ok().body(result, Employee::class.java)
+    }
+
 }
