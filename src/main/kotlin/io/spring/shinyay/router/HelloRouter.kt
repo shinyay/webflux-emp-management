f714efbab6c349b7f4fbee1bf381bb4d2a11a67b
shinyay <shinya.com@gmail.com>
2021-11-15 14:10:01 +0900

    Add handler class to router

--- src/main/kotlin/io/spring/shinyay/router/HelloRouter.kt
+++ src/main/kotlin/io/spring/shinyay/router/HelloRouter.kt
@@ -1,7 +1,24 @@
 package io.spring.shinyay.router
 
+import io.spring.shinyay.handler.HelloHandler
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Flux
+
 
 @Configuration
 class HelloRouter {
+
+    @Bean
+    fun hello(handler: HelloHandler): RouterFunction<ServerResponse> {
+        return route()
+            .GET("/hello/mono", handler::helloMono)
+            .GET("/hello/flux", handler::helloFlux)
+            .GET("/hello/{name}", handler::helloName)
+            .build()
+    }
 }
