1b96f4c657ea14b9dbb97700d10d9d2107d66718
shinyay <shinya.com@gmail.com>
2021-11-15 11:57:44 +0900

    Add Mono handler sample

--- src/main/kotlin/io/spring/shinyay/handler/HelloHandler.kt
+++ src/main/kotlin/io/spring/shinyay/handler/HelloHandler.kt
@@ -1,7 +1,19 @@
 package io.spring.shinyay.handler
 
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 @Component
 class HelloHandler {
+
+    fun helloMono(request: ServerRequest): Mono<ServerResponse> {
+        return ServerResponse.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(
+                Mono.just("Hello Spring Boot!"),String::class.java
+            )
+    }
 }
